3dd34c1d0579cc3eb7bdf63a0b8bd78af3c9558a
majins12 <71135573+majins12@users.noreply.github.com>
2020-09-11 20:57:50 +0700

    Add files via upload

---
+++ AjiUts.java
@@ -0,0 +1,42 @@
+
+package ajiuts;
+
+
+public class AjiUts {
+
+ 
+    public static void main(String[] args) {
+
+   
+        String a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-" ;
+        char hasil = a.charAt(0);
+        
+        char b = a.charAt(12);
+        char c = a.charAt(27);
+        char d = a.charAt(0);
+        char e = a.charAt(9);
+        char f = a.charAt(8);
+        char g = a.charAt(27);
+        char h = a.charAt(13);
+        char i = a.charAt(27);
+         char j = a.charAt(18);
+       
+ 
+         System.out.print(b);
+         System.out.print(c);
+         System.out.print(d);
+         System.out.print(e);
+         System.out.print(f);
+         System.out.print(g);
+         System.out.print(h);
+         System.out.print(i);
+         System.out.println(j);
+        
+             
+     
+
+
+
+    }
+    
+}
